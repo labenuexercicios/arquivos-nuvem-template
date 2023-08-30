cfeb4c7b45c24680dddd290f6b48c28ce2aa0054
yuzo <yuzokamoto@gmail.com>
2023-08-30 09:04:43 -0300

    update

--- database.sql
+++ database.sql
@@ -1,4 +1,4 @@
--- Active: 1684866303071@@127.0.0.1@3306
+-- Active: 1693396814975@@127.0.0.1@3306
 CREATE TABLE users (
     id TEXT PRIMARY KEY UNIQUE NOT NULL,
     name TEXT NOT NULL,
@@ -12,10 +12,8 @@ INSERT INTO users (id, name, email, password, role)
 VALUES
   -- tipo NORMAL e senha = fulano123
 	('u001', 'Fulano', 'fulano@email.com', '$2a$12$qPQj5Lm1dQK2auALLTC0dOWedtr/Th.aSFf3.pdK5jCmYelFrYadC', 'NORMAL'),
-
   -- tipo NORMAL e senha = beltrana00
 	('u002', 'Beltrana', 'beltrana@email.com', '$2a$12$403HVkfVSUbDioyciv9IC.oBlgMqudbnQL8ubebJIXScNs8E3jYe2', 'NORMAL'),
-
   -- tipo ADMIN e senha = astrodev99
 	('u003', 'Astrodev', 'astrodev@email.com', '$2a$12$lHyD.hKs3JDGu2nIbBrxYujrnfIX5RW5oq/B41HCKf7TSaq9RgqJ.', 'ADMIN');
 
